b57e9dd45235083a1fcfa0b5ff8609681a4a46e3
醨 <2690931889@qq.com>
2023-12-10 03:10:18 +0800

    添加检索摄像头功能、绑定spinbox和Slider数值变动、信息提示框

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ import time
 import cv2
 
 from PyQt5.QtCore import Qt, QPoint, QTimer, QThread, pyqtSignal
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMenu, QAction
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMenu, QAction, QLabel, QVBoxLayout, QWidget
 from PyQt5.QtGui import QImage, QPixmap, QPainter, QIcon
 from PyQt5 import uic
 from mainUI.mainUI import Ui_MainWindow
+from toolUI.TipsMessageBox import TipsMessageBox
+from toolUI.cameranums import Camera
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -37,8 +39,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.fileButton.clicked.connect(self.open_file)
 
         self.cameraButton.setCheckable(True)
-        self.cameraButton.clicked.connect(self.toggle_camera)
+        self.cameraButton.clicked.connect(self.chose_cam)
+
         # 初始化摄像头和计时器
+        self.cam = '0'
         self.capture = None
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_frame)
@@ -64,27 +68,119 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         open_fold = config['Default_path']
         if not os.path.exists(open_fold):
             open_fold = os.getcwd()
-        name, _ = QFileDialog.getOpenFileName(self, 'Video/image', open_fold, "Pic_File(*.mp4 *.mkv *.avi *.flv "
+        name, _ = QFileDialog.getOpenFileName(self, 'Video/image', open_fold, "图像或视频文件(*.mp4 *.mkv *.avi *.flv "
                                                                               "*.jpg *.png)")
         if name:
             self.det_thread.source = name
-            self.statistic_msg('Loaded file：{}'.format(os.path.basename(name)))
+            self.bottom_msg('Loaded file：{}'.format(os.path.basename(name)))
             config['Default_path'] = os.path.dirname(name)
             config_json = json.dumps(config, ensure_ascii=False, indent=2)
             with open(config_file, 'w', encoding='utf-8') as f:
                 f.write(config_json)
             self.stop()
 
-    def toggle_camera(self):
-        if self.cameraButton.isChecked():
-            self.start_camera()
+    # def toggle_camera(self):
+    #     if self.cameraButton.isChecked():
+    #         self.start_camera()
+    #     else:
+    #         self.stop_camera()
+    #
+    # def start_camera(self):
+    #     if self.capture is None:
+    #         self.capture = cv2.VideoCapture(0)  # 打开默认摄像头（索引为0）
+    #     self.timer.start(20)  # 设置更新画面的时间间隔
+    #
+    # def stop_camera(self):
+    #     self.timer.stop()
+    #     if self.capture is not None:
+    #         self.capture.release()  # 释放摄像头
+    #         self.capture = None  # 将 self.capture 设为 None
+    #     self.label_previous.clear()
+    #
+    # def update_frame(self):
+    #     ret, frame = self.capture.read()  # 读取摄像头画面
+    #
+    #     if ret:
+    #         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # 将图像格式从 OpenCV 格式转换为 RGB 格式
+    #         h, w, ch = frame.shape
+    #         bytes_per_line = ch * w
+    #         convert_to_Qt_format = QImage(frame.data, w, h, bytes_per_line, QImage.Format_RGB888)
+    #         pixmap = QPixmap.fromImage(convert_to_Qt_format)
+    #         self.label_previous.setPixmap(pixmap.scaled(self.label_previous.size()))  # 在 QLabel 中显示画面
+    #
+    # def closeEvent(self, event):
+    #     if self.capture is not None:
+    #         self.capture.release()  # 释放摄像头
+    #     event.accept()
+
+    def change_val(self, x, flag):
+        if flag == 'confSpinBox':
+            self.confSlider.setValue(int(x * 100))
+        elif flag == 'confSlider':
+            self.confSpinBox.setValue(x / 100)
+            # self.det_thread.conf_thres = x / 100
+        elif flag == 'iouSpinBox':
+            self.iouSlider.setValue(int(x * 100))
+        elif flag == 'iouSlider':
+            self.iouSpinBox.setValue(x / 100)
+            # self.det_thread.iou_thres = x / 100
+        elif flag == 'rateSpinBox':
+            self.rateSlider.setValue(x)
+        elif flag == 'rateSlider':
+            self.rateSpinBox.setValue(x)
+            # self.det_thread.rate = x * 10
         else:
+            pass
+
+    def chose_cam(self):
+        if self.cameraButton.isChecked():
+            try:
+                TipsMessageBox(
+                    self.closeButton, title='Tips', text='Loading camera', time=2000, auto=True).exec_()
+                # get the number of local cameras
+                _, cams = Camera().get_cam_num()
+                popMenu = QMenu()
+                popMenu.setFixedWidth(self.cameraButton.width())
+                popMenu.setStyleSheet('''
+                                                QMenu {
+                                                font-size: 16px;
+                                                font-family: "Microsoft YaHei UI";
+                                                font-weight: light;
+                                                color:white;
+                                                border-style: solid;
+                                                border-width: 0px;
+                                                border-color: rgba(255, 255, 255, 255);
+                                                border-radius: 3px;
+                                                background-color: rgba(200, 200, 200,50);}
+                                                ''')
+
+                for cam in cams:
+                    exec("action_%s = QAction('%s')" % (cam, cam))
+                    exec("popMenu.addAction(action_%s)" % cam)
+
+                x = self.groupBox_input.mapToGlobal(self.cameraButton.pos()).x()
+                y = self.groupBox_input.mapToGlobal(self.cameraButton.pos()).y()
+                y = y + self.cameraButton.frameGeometry().height()
+                pos = QPoint(x, y)
+                action = popMenu.exec_(pos)
+                if action:
+                    self.cam = action.text()
+                    self.start_camera()
+                    self.bottom_msg('Loading camera：{}'.format(action.text()))
+            except Exception as e:
+                self.bottom_msg('%s' % e)
+        else:
+            TipsMessageBox(
+                self.closeButton, title='Tips', text='closing camera', time=2000, auto=True).exec_()
             self.stop_camera()
+            self.bottom_msg('')
 
     def start_camera(self):
         if self.capture is None:
-            self.capture = cv2.VideoCapture(0)  # 打开默认摄像头（索引为0）
-        self.timer.start(20)  # 设置更新画面的时间间隔
+            camera_index = int(self.cam)  # 获取选择的摄像头索引
+            if camera_index >= 0:
+                self.capture = cv2.VideoCapture(camera_index)  # 打开选择的摄像头
+                self.timer.start(20)  # 设置更新画面的时间间隔
 
     def stop_camera(self):
         self.timer.stop()
@@ -95,7 +191,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def update_frame(self):
         ret, frame = self.capture.read()  # 读取摄像头画面
-
         if ret:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # 将图像格式从 OpenCV 格式转换为 RGB 格式
             h, w, ch = frame.shape
@@ -109,21 +204,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.capture.release()  # 释放摄像头
         event.accept()
 
-    def change_val(self, x, flag):
-        if flag == 'confSpinBox':
-            self.confSlider.setValue(int(x * 100))
-        elif flag == 'confSlider':
-            self.confSpinBox.setValue(x / 100)
-        elif flag == 'iouSpinBox':
-            self.iouSlider.setValue(int(x * 100))
-        elif flag == 'iouSlider':
-            self.iouSpinBox.setValue(x / 100)
-        elif flag == 'rateSpinBox':
-            self.rateSlider.setValue(x)
-        elif flag == 'rateSlider':
-            self.rateSpinBox.setValue(x)
-        else:
-            pass
+    def bottom_msg(self, msg):
+        self.label_bottom.setText(msg)
+        # self.qtimer.start(3000)
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,40 +1,97 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QSpinBox, QSlider, QWidget
+import cv2
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QGroupBox, QLabel, QWidget, QComboBox
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QPixmap, QImage
 
-class SpinBoxSliderWindow(QMainWindow):
+class CameraWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("SpinBox and Slider")
+        self.setWindowTitle("Camera Viewer")
+
+        # 创建主布局和窗口部件
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
         self.layout = QVBoxLayout()
         self.central_widget.setLayout(self.layout)
 
-        self.spin_box = QSpinBox()
-        self.slider = QSlider()
+        # 创建摄像头选择下拉菜单
+        self.camera_combo = QComboBox()
+        self.camera_combo.addItem("Select Camera")
+        self.detect_camera_devices()  # 检测摄像头设备
+        self.layout.addWidget(self.camera_combo)
 
-        # 设置取值范围
-        min_value = 0
-        max_value = 100
-        self.spin_box.setRange(min_value, max_value)
-        self.slider.setRange(min_value, max_value)
+        # 创建按钮
+        self.btn_toggle_camera = QPushButton("Start Camera")
+        self.btn_toggle_camera.setCheckable(True)  # 设置按钮为可选中状态
+        self.btn_toggle_camera.clicked.connect(self.toggle_camera)
+        self.layout.addWidget(self.btn_toggle_camera)
 
-        # 初始值设定
-        initial_value = 50
-        self.spin_box.setValue(initial_value)
-        self.slider.setValue(initial_value)
+        # 创建用于显示摄像头画面的 QLabel
+        self.camera_groupbox = QGroupBox("Camera View")
+        self.camera_layout = QVBoxLayout()
+        self.camera_label = QLabel()
+        self.camera_label.setFixedSize(640, 480)  # 设置显示画面的大小
+        self.camera_layout.addWidget(self.camera_label)
+        self.camera_groupbox.setLayout(self.camera_layout)
+        self.layout.addWidget(self.camera_groupbox)
 
-        # 将信号和槽连接起来
-        self.spin_box.valueChanged.connect(self.slider.setValue)
-        self.slider.valueChanged.connect(self.spin_box.setValue)
+        # 初始化摄像头和计时器
+        self.capture = None
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_frame)
 
-        self.layout.addWidget(self.spin_box)
-        self.layout.addWidget(self.slider)
+    def detect_camera_devices(self):
+        for i in range(10):  # 检测摄像头设备，可根据实际情况调整范围
+            cap = cv2.VideoCapture(i)
+            if cap.isOpened():
+                self.camera_combo.addItem(f"Camera {i}")
+                cap.release()
+
+    def toggle_camera(self):
+        if not self.btn_toggle_camera.isChecked():
+            self.stop_camera()
+            self.camera_combo.setEnabled(True)
+        else:
+            self.start_camera()
+            self.camera_combo.setEnabled(False)
+
+    def start_camera(self):
+        if self.capture is None:
+            camera_index = self.camera_combo.currentIndex() - 1  # 获取选择的摄像头索引
+            if camera_index >= 0:
+                self.capture = cv2.VideoCapture(camera_index)  # 打开选择的摄像头
+                self.timer.start(20)  # 设置更新画面的时间间隔
+                self.btn_toggle_camera.setText("Stop Camera")
+
+    def stop_camera(self):
+        self.timer.stop()
+        if self.capture is not None:
+            self.capture.release()  # 释放摄像头
+            self.capture = None  # 将 self.capture 设为 None
+        self.camera_label.clear()
+        self.btn_toggle_camera.setText("Start Camera")
+
+    def update_frame(self):
+        ret, frame = self.capture.read()  # 读取摄像头画面
+
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # 将图像格式从 OpenCV 格式转换为 RGB 格式
+            h, w, ch = frame.shape
+            bytes_per_line = ch * w
+            convert_to_Qt_format = QImage(frame.data, w, h, bytes_per_line, QImage.Format_RGB888)
+            pixmap = QPixmap.fromImage(convert_to_Qt_format)
+            self.camera_label.setPixmap(pixmap.scaled(self.camera_label.size()))  # 在 QLabel 中显示画面
+
+    def closeEvent(self, event):
+        if self.capture is not None:
+            self.capture.release()  # 释放摄像头
+        event.accept()
 
 def main():
     app = QApplication(sys.argv)
-    window = SpinBoxSliderWindow()
+    window = CameraWindow()
     window.show()
     sys.exit(app.exec_())
 
